a507c1b10bd3d7faec3302cbf446fbbbf7ef8ccf
Lev <lguzm038@uottawa.ca>
2021-05-14 13:23:33 -0400

    Structured project folders

--- flutter_amplify/lib/main.dart
+++ flutter_amplify/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:amplify_flutter/amplify.dart';
 import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-import 'amplifyconfiguration.dart';
+import './amplify/amplifyconfiguration.dart';
 
 void main() {
   runApp(MyApp());
